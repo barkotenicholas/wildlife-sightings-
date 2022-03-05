356ecf0e04bfd5d0b41c441cccf4515a79d33717
barkotenicholas <barkotenicholas@gmail.com>
2022-03-05 10:10:31 +0300

    Added endagered animal instantiation test

--- src/main/java/model/EndangeredAnimal.java
+++ src/main/java/model/EndangeredAnimal.java
@@ -1,4 +1,15 @@
 package model;
 
-public class EndangeredAnimal {
+public class EndangeredAnimal  extends Animal{
+
+    private String health;
+    private String age;
+
+
+    public EndangeredAnimal(String animalName, String health, String age) {
+        super(animalName);
+        this.health = health;
+        this.age = age;
+    }
+
 }
--- src/test/java/model/EndangeredAnimalTest.java
+++ src/test/java/model/EndangeredAnimalTest.java
@@ -1,7 +1,16 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EndangeredAnimalTest {
 
+    @Test
+    @DisplayName("Endangered animal instantiates correctly")
+    public void endangered_instantiatesCorrectly_true(){
+        EndangeredAnimal endangeredAnimal  = new EndangeredAnimal("Tiger","healthy","newborn");
+        assertNotNull(endangeredAnimal);
+    }
 }
